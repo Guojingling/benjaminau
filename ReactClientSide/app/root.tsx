fab5f81a48602430b0407ec58a71ffa4b25303a1
Jing Guo <guojingling@hotmail.com>
2025-04-10 21:26:18 +1000

    Add bootstrap and cafe images.

--- ReactClientSide/app/root.tsx
+++ ReactClientSide/app/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.css";
+//import "./app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -20,6 +20,20 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css",
+    integrity: "sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7",
+    crossorigin: "anonymous"
+  },
+];
+
+export const scripts: Route.ScriptsFunction = () => [
+  {
+    src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js",
+    integrity: "sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq",
+    crossOrigin: "anonymous", 
   },
 ];
 
--- ReactClientSide/app/welcome/welcome.tsx
+++ ReactClientSide/app/welcome/welcome.tsx
@@ -1,12 +1,15 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+// import logoDark from "./logo-dark.svg";
+// import logoLight from "./logo-light.svg";
+
+import logoDark from "./cafefront.jpg";
+import logoLight from "./cafeinside.jpg";
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
+    <main className="container text-center">
+      <div className="row justify-content-center">
+        <header className="">
+          <div className="col">
             <img
               src={logoLight}
               alt="React Router"
@@ -19,16 +22,16 @@ export function Welcome() {
             />
           </div>
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+        <div className="row justify-content-center">
+          <nav className="col">
+            <p className="fs-1 text-center">
               What&apos;s next?
             </p>
             <ul>
               {resources.map(({ href, text, icon }) => (
                 <li key={href}>
                   <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
+                    className="text-blue-700 hover:underline dark:text-blue-500"
                     href={href}
                     target="_blank"
                     rel="noreferrer"
@@ -48,8 +51,8 @@ export function Welcome() {
 
 const resources = [
   {
-    href: "https://reactrouter.com/docs",
-    text: "React Router Docs",
+    href: "https://www.meandu.app/bashan?utm_source=qr",
+    text: "Bashan cafe menu",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -68,8 +71,8 @@ const resources = [
     ),
   },
   {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
+    href: "https://www.facebook.com/artisancanberra/",
+    text: "Facebook page",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
