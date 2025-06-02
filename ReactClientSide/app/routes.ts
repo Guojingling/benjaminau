9036d6d425f02e1e5858f6cc49a8f7c53e0f9cb5
Jing Guo <guojingling@hotmail.com>
2025-06-02 17:11:21 +1000

    Revert "Add order route"
    This reverts commit 2eabacaf81c9c3f5347e7456e395aa0bd00f2a41.

--- ReactClientSide/app/routes.ts
+++ ReactClientSide/app/routes.ts
@@ -1,5 +1,3 @@
-import { type RouteConfig, route, index } from "@react-router/dev/routes";
+import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"),
-                route("orders", "./orders/orders.tsx"),
-] satisfies RouteConfig;
+export default [index("routes/home.tsx")] satisfies RouteConfig;
