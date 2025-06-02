5e12514a042fcf60aa2f2b082b7786e500b27d27
Jing Guo <guojingling@hotmail.com>
2025-06-02 20:17:07 +1000

    Add cafe menu

--- AppAPI/artisancafeAPI/Program.cs
+++ AppAPI/artisancafeAPI/Program.cs
@@ -21,7 +21,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -33,9 +33,24 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+var cafeMenu = new CafeMenulist[] {
+    new(1,  "burgerwitheggs", "burgerwitheggs.jpg"),
+    new(2,  "petty", "petty.jpg"),
+    new(2,  "prowndish", "prowndish.jpg"),
+};
+
+var cafemenuApi = app.MapGroup("/cafemenu");
+cafemenuApi.MapGet("/", () => cafeMenu);
+cafemenuApi.MapGet("/{id}", (int id) =>
+    cafeMenu.FirstOrDefault(a => a.Id == id) is { } memu
+        ? Results.Ok(memu)
+        : Results.NotFound());
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+record CafeMenulist(int Id, string? DishName, string? ImageName, DateOnly? ValidUntil = null, bool IsShown = true);
